68c8f5aaa3424a803541ffca773fee26e41de6b3
Alberto Garcia <albertoinformatica97@gmail.com>
2019-10-17 23:34:37 +0200

    Se crea y muestra la tabla con la cabecera

---
+++ assets/js/index.js
@@ -0,0 +1,54 @@
+function createElements(type, numElements) {
+
+  let elements = []
+
+  for (let i = 0; i < numElements; i++) {
+    elements.push(document.createElement(type))
+  }
+
+  return elements
+
+}
+
+function assignValuesToElements(elements, values) {
+
+  for (let i = 0; i < elements.length; i++) {
+    elements[i].textContent = values[i]
+  }
+
+}
+
+function appendChilds(element, elementsToAppend) {
+
+  for (let i = 0; i < elementsToAppend.length; i++) {
+    element.appendChild(elementsToAppend[i])
+  }
+
+}
+
+function addClassToElements(elements, classString) {
+
+  for (let i = 0; i < elements.length; i++) {
+    elements[i].classList.add(classString)
+  }
+
+}
+
+function generateTable() {
+
+  let table = document.createElement('table')
+  let trTitles = document.createElement('tr')
+  let thTitles = [thTitle1, thTitle2, thTitle3, thTitle4, thTitle5] = createElements('th', 5)
+  let thTitleValues = ['Título', 'Servicio', 'Página', 'Puerto', 'Enlace']
+
+  addClassToElements(thTitles, 't-align')
+  addClassToElements(thTitles, 'title-table')
+  assignValuesToElements(thTitles, thTitleValues)
+  appendChilds(trTitles, thTitles)
+  table.appendChild(trTitles)
+
+  // TODO bucle para rellenar la tabla a partir del array data del fichero data.js
+  
+  document.getElementById('containerTable').appendChild(table)
+
+}
